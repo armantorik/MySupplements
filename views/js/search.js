68d110a1d6e51de2ed479f824f42277a3e37aabf
s21artunc <artunc@sabanciuniv.edu>
2021-06-10 05:10:29 +0300

    search (frontend)

---
+++ views/js/search.js
@@ -0,0 +1,56 @@
+async function dosearch(){
+
+
+    const response = await fetch('/products/search/'+ $("#searchinput").val());
+
+    const myJson = await response.json(); //extract JSON from the http response
+
+    myJson.proArr.forEach(pro => {
+
+
+       
+        document.getElementById("searchid").innerHTML += `
+        <a class="dropdown-item" href="html/product.html?pid=${pro.id}"> 
+        
+          <th>${pro.product.name}</th>
+          <th>${pro.product.price}</th>
+        </a>
+
+        `
+      });
+}
+
+async function dossssearch(){
+
+
+queryurl = "/products/search/" + $("#searchinput").val();
+
+axios.get(queryurl).then(
+ (response) => {
+    alert(response)
+  myJson = response.json();
+  console.log('Processing Request');
+
+  console.log(response)
+    console.log($("#searchinput").val())
+    alert(myJson)
+    myJson.proArr.forEach(pro => {
+
+        console.log(pro.id)
+        alert(pro.id)
+       
+        document.getElementById("searchid").innerHTML += `
+        <a class="dropdown-item" href="/products?pid=${pro.id}"> 
+        
+          <th>${pro.product.name}</th>
+          <th>${pro.porduct.price}</th>
+        </a>
+
+        `
+},
+(error) => {
+    reject(error);
+    }
+);
+})
+}
