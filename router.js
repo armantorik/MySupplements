c11971c9704be9c2a57b9854d578093d27951890
s21armantorikoglu <armantorikoglu@sabanciuniv.edu>
2021-08-17 01:49:51 +0300

    del router

--- router.js
+++
@@ -1,8 +0,0 @@
-
-// const express = require('express');
-// const router = express.Router();
-// const firebase = require('firebase');
-// router.all("/about", (req, res, next) => {
-//      pid = req.params.pid;
-// });
-// module.exports = router;
